d45074bb3716611f50669e75b2603d7ff024e920
marisolkarina <49735733+marisolkarina@users.noreply.github.com>
2024-01-14 11:31:20 -0500

    Crear tabla comentario e inserciones

---
+++ Tarea3/creacionComentario.sql
@@ -0,0 +1,22 @@
+create table if not exists comentario(
+    comentario_id serial primary key,
+    num_reacciones integer not null,
+    contenido text not null,
+
+    post_id_fk integer,
+    usuario_id_fk integer
+);
+
+alter table comentario
+add constraint fk_post
+foreign key(post_id_fk)
+references post(post_id);
+
+alter table usuario
+add constraint fk_usuario
+foreign key(usuario_id_fk)
+references usuario(usuario_id)
+on delete set null;
+
+select * from comentario
+
